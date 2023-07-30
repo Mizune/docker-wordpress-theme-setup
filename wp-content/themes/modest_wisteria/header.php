4b89d778935af2671a6e0b17094da280493619c6
m1zyuk1 <me@mizune.ms>
2023-07-30 12:05:17 +0900

    Change head to apply bootstrap

--- wp-content/themes/modest_wisteria/header.php
+++ wp-content/themes/modest_wisteria/header.php
@@ -13,10 +13,13 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- <meta charset="<?php bloginfo( 'charset' ); ?>"> -->
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+ 
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
 	<?php wp_head(); ?>
 </head>
 
