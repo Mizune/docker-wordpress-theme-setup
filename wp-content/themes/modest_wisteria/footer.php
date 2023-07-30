5d5125012b5ccf650ef61a1e19d0909a95a6cf0d
m1zyuk1 <me@mizune.ms>
2023-07-30 13:00:28 +0900

    Apply bootstrap container

--- wp-content/themes/modest_wisteria/footer.php
+++ wp-content/themes/modest_wisteria/footer.php
@@ -11,8 +11,8 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
+	<footer id="colophon" class="site-footer container-fluid">
+		<div class="site-info text-center">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'modest_wisteria' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
@@ -22,7 +22,7 @@
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'modest_wisteria' ), 'modest_wisteria', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'modest_wisteria' ), 'modest_wisteria', '<a href="https://mizune.ms/">M1zyuk1</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
--- wp-content/themes/modest_wisteria/header.php
+++ wp-content/themes/modest_wisteria/header.php
@@ -25,7 +25,7 @@
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-<div id="page" class="site">
+<div id="page" class="site container-fluid">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'modest_wisteria' ); ?></a>
 
 	<header id="masthead" class="site-header">
